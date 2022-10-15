624e6fe27cfe40349b6a5d8a4ed0a3495d423e55
abluey <zoe1615@hotmail.com>
2022-10-15 14:30:51 +0100

    feat: set up simple menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button startGameBtn;
+    [SerializeField] private string startSceneName;
+
+    // Start is called before the first frame update
+    void Start() {
+        Debug.Log($"Button is null? {startGameBtn == null}");
+        Debug.Log($"Button.OnClick is null? {startGameBtn.onClick == null}");
+        startGameBtn.onClick.AddListener(startGame);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void startGame() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(startSceneName, LoadSceneMode.Additive);
+        asyncLoad.completed += onStartGame;
+    }
+
+    private void onStartGame(AsyncOperation loadOperation) {
+        _ = SceneManager.UnloadSceneAsync(gameObject.scene);
+    }
+}
