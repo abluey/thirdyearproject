3b1f705c1524e963f30b9c329a3aaef98770189f
abluey <zoe1615@hotmail.com>
2023-02-27 22:42:43 +0000

    feat: start on presents

--- Assets/Scripts/Laptop/Birthday/PresentPage.cs
+++ Assets/Scripts/Laptop/Birthday/PresentPage.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class PresentPage : MonoBehaviour
 {
     [SerializeField] private Image presImage;
+    [SerializeField] private Image shooImage;
     [SerializeField] private TMPro.TMP_Text boughtText;
     private string presName;
 
@@ -14,6 +15,8 @@ public class PresentPage : MonoBehaviour
     void OnEnable() {
         presName = BirthdayManager.clickedName;
         boughtText.gameObject.SetActive(false);
+        shooImage.gameObject.SetActive(false);
+        presImage.gameObject.SetActive(false);
 
         if (PlayerChoices.buyPresent) {
             buyBtn.gameObject.SetActive(false);
@@ -21,18 +24,24 @@ public class PresentPage : MonoBehaviour
 
         switch (presName) {
             case "pres1":
-                BirthdayManager.title.text = "Pres 1";
+                BirthdayManager.title.text = "Birth Day presents:\nCandy Hearts";
                 break;
             case "pres2":
-                BirthdayManager.title.text = "Pres 2";
+                BirthdayManager.title.text = "Birth Day presents:\nFresh Flowers";
                 break;
             case "pres3":
-                BirthdayManager.title.text = "Pres 3";
+                BirthdayManager.title.text = "Birth Day presents:\nFashionista Shoes";
                 break;
             default: Debug.Log("Something went wrong"); break;
         }
 
-        presImage.sprite = Resources.Load<Sprite>($"Sprites/{presName}");
+        if (presName == "pres3") {
+            shooImage.gameObject.SetActive(true);
+        } else {
+            presImage.sprite = Resources.Load<Sprite>($"Sprites/{presName}");
+            presImage.gameObject.SetActive(true);
+        }
+        
     }
 
     void Start()
